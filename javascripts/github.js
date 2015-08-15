86b878366ace4ddda9fce68be83af72f3bc339f9
5n4k3 K33p3r <5n4k3@snakepitsoftware.com>
2015-08-15 12:14:32 -0400

    Marked the javascript as MIT Licensed Signed-off-by: 5n4k3 K33p3r <5n4k3@snakepitsoftware.com>

--- javascripts/github.js
+++ javascripts/github.js
@@ -1,3 +1,28 @@
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Snakepit Software
+// Author: Rodney C Forbes    <5n4k3@snakepitsoftware.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
 // type 1 : repos of a user
 // type 2 : repos of an organization
 jQuery.githubRepos = function(user, type, callback) {
@@ -22,7 +47,7 @@ jQuery.fn.loadRepositories = function(user, type) {
 
     sortByNumberOfWatchers(repos);
     target.empty().append(list);
-    
+
     $(repos).each(function() {
       list.append('<dt><a href="'+ this.html_url +'">' + this.name + '</a></dt>');
       list.append('<dd>' + this.description + '</dd>');
@@ -54,11 +79,11 @@ jQuery.fn.loadMembers = function(organization) {
     var list = $('<dl/>');
 
     target.empty().append(list);
-    
+
     $(members).each(function() {
       $.githubUser(this.login, function(data) {
         var user = data;
-        
+
         list.append('<dt><a href="'+ user.html_url +'">' + user.name + '</a></dt>');
         list.append('<dd>' + user.bio + '</dd>');
       });
